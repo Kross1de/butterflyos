bd9c66338fc01666fa77358d14c9fdfb8a6a1bb9
Kross1de <awesome.hubygu@yandex.ru>
2025-05-13 16:49:47 +0600

    Kernel: printf Implementation

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir binaries/
 nasm -f elf32 source/bootloader.asm -o binaries/bootloader.o
-gcc -m32 -ffreestanding -c source/kernel.c -o binaries/kernel.o
+gcc -m32 -ffreestanding -fno-stack-protector -c source/kernel.c -o binaries/kernel.o
 ld -m elf_i386 -o kernel.bin -T source/linker.ld binaries/bootloader.o binaries/kernel.o
 qemu-system-i386 -kernel kernel.bin
 rm -rf kernel.bin binaries/
