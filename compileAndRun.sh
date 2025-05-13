59e9866ca95754713a1ac7a9433e91b232b24429
Kross1de <awesome.hubygu@yandex.ru>
2025-05-13 17:23:44 +0600

    separation

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -2,6 +2,7 @@
 mkdir binaries/
 nasm -f elf32 source/bootloader.asm -o binaries/bootloader.o
 gcc -m32 -ffreestanding -fno-stack-protector -c source/kernel.c -o binaries/kernel.o
-ld -m elf_i386 -o kernel.bin -T source/linker.ld binaries/bootloader.o binaries/kernel.o
+gcc -m32 -ffreestanding -fno-stack-protector -c source/printf.c -o binaries/printf.o
+ld -m elf_i386 -o kernel.bin -T source/linker.ld binaries/bootloader.o binaries/kernel.o binaries/printf.o
 qemu-system-i386 -kernel kernel.bin
 rm -rf kernel.bin binaries/
---
+++ source/printf.h
@@ -0,0 +1,9 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+void printf(const char *format, ...);
+static void printChar(char *videomem, char c, char attr);
+static void printString(char *videomem, const char *str, char attr);
+static void printInt(char *videomem, int num, int base,  char attr);
+static char *itoa(int value, char *str, int base);
+static int strlen(const char *str);
+#endif
