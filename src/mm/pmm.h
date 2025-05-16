21bc13971c7c909dcb4f3ccb770799b7345c01e8
Kross1de <awesome.hubygu@yandex.ru>
2025-05-16 16:51:32 +0600

    m  o r  e   w o  r   k  on  P m   m

--- src/mm/pmm.h
+++ src/mm/pmm.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <types.h>
+#include <kernel.h>
 
 #define PAGE_SIZE 4096
 #define divCeiling(x,y)(x+(y-1))/y
 #define alignUp(x,y)divCeiling(x,y)*y
 #define alignDown(x,y)(x/y)*y
+#define higherHalf(ptr) ((void*)((u64)ptr)+hhdm_offset)
+#define physical(ptr) ((void*)((u64)ptr)-hhdm_offset)
 
 void pmm_init();
